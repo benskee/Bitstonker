b750ff49a95fc6ed8cf6f83128ea10cfed2bcae8
benskee <benskee33@gmail.com>
2021-02-05 10:47:48 -0500

    mismatch column

--- app/test.py
+++ app/test.py
@@ -14,5 +14,5 @@ df_btc = pd.read_csv('csv/btc.csv')
 update_alpha = requests.get(f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={api_key}')
 update_df = update_alpha.json()
 df_update = pd.DataFrame.from_dict(update_df['Realtime Currency Exchange Rate'],orient='index')
-df_btc.loc[len(df_btc)] = [str(datetime.now())[:10], float(df_update[0][4])]
+df_btc.loc[len(df_btc)] = [len(df_btc), str(datetime.now())[:10], float(df_update[0][4])]
 df_btc.to_csv(rf'test/btc.csv')
