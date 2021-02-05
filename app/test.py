b35fc8d7056eba13424f7229f21277d6f78c635c
benskee <benskee33@gmail.com>
2021-02-05 09:50:57 -0500

    test/btc

--- app/test.py
+++ app/test.py
@@ -4,12 +4,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
-from dotenv import load_dotenv
 
-df_btc = pd.read_csv(f'app/test/btc.csv')
+api_key = os.environ['SECRET_KEY']
+
+df_btc = pd.DataFrame(columns=['date', 'close_price'])
 
 update_alpha = requests.get(f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={api_key}')
 update_df = update_alpha.json()
 df_update = pd.DataFrame.from_dict(update_df['Realtime Currency Exchange Rate'],orient='index')
 df_btc.loc[len(df_btc)] = [str(datetime.now())[:10], float(df_update[0][4])]
-df_btc.to_csv(rf'app/test/btc.csv')
+df_btc.to_csv(rf'test/btc.csv')
