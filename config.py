55bd77573038de549fd1e2af74d5b3f3ab9ac0c2
benskee <benskee33@gmail.com>
2021-02-22 19:50:03 -0500

    elesql

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+    SQLALCHEMY_DATABASE_URI = 'postgres://jqfaanmd:xkQ9pPvrHz2cNeTxRcfrSxy6Ayq_uN_j@ziggy.db.elephantsql.com:5432/jqfaanmd'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
