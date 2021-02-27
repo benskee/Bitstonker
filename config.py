07642a741f25b15b3c1f5a72457c18c005bb3c9e
benskee <benskee33@gmail.com>
2021-02-27 13:43:56 -0500

    config

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-# .env
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
